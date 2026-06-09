d2f80d6648f5618da1466dcbed90535a2d56b9e4
Leonard Göhrs <l.goehrs@pengutronix.de>
2026-06-09 20:52:17 +0200

    machines: mac_pool: reuse MAC addresses that have been assigned before
    The MAC addresses we use only have to be unique at that specific point in
    time. When assigning a random MAC to every VM we can easily cause
    (temporary) exhaustion of available IPv4 addresses from a DHCP server,
    since the leases need time to expire.
    
    Reuse MAC addresses so that DHCP servers can give out the same IP to
    different VMs (as long as they are not running at the same time).
    
    Signed-off-by: Leonard Göhrs <l.goehrs@pengutronix.de>

--- src/machines/mac_pool.rs
+++ src/machines/mac_pool.rs
@@ -1,15 +1,27 @@
+use std::sync::Mutex;
+
 use rand::RngExt;
 
 const MAC_MASK: u64 = 0xfe_ff_ff_ff_ff_ff;
 const LOCAL_BIT: u64 = 0x02_00_00_00_00_00;
 
+static RETURNED: Mutex<Vec<u64>> = Mutex::new(Vec::new());
+
 pub struct Mac {
     addr: u64,
 }
 
-/// Get a random MAC address
+/// MAC Address pool with automatic reuse
+///
+/// Forrest needs to assign MAC addresses to virtual machines that are unique
+/// at that point in time. We do hower want to reuse the MACs once they are no
+/// longer used, to make sure we do not run out of DHCP leases.
 pub fn get_mac() -> Mac {
-    let addr: u64 = rand::rng().random();
+    let addr = RETURNED
+        .lock()
+        .unwrap()
+        .pop()
+        .unwrap_or_else(|| rand::rng().random());
     let addr = (addr | LOCAL_BIT) & MAC_MASK;
 
     Mac { addr }
@@ -24,6 +36,12 @@ impl std::fmt::Display for Mac {
         }
 
         write!(f, "{:02x}", bytes[7])
+    }
+}
+
+impl Drop for Mac {
+    fn drop(&mut self) {
+        RETURNED.lock().unwrap().push(self.addr);
     }
 }
 
